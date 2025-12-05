289b2569c90a488c2f11135faed10b52657c4f78
adamkvasniak <114985439+adamkvasniak@users.noreply.github.com>
2025-12-05 18:27:07 +0100

    Update buzzerTest.cpp

--- buzzerTest.cpp
+++ buzzerTest.cpp
@@ -1,17 +1,16 @@
 #include "mbed.h"
 
-PwmOut buzzer(PC_8);     // PWM výstup na buzzer
-DigitalIn button(PC_13); // USER BUTTON B1
+DigitalOut buzzer(PC_8); 
+DigitalIn button(PC_13); // B1
 
 int main() {
-    button.mode(PullUp);        // B1 je aktívny v log. 0
-    buzzer.period(1.0f / 2000); // 2 kHz tón
+    button.mode(PullUp);  // B1: pressed = 0
 
     while (1) {
-        if (button.read() == 0) {   // Stlačené
-            buzzer.write(0.5f);     // 50% hlasitosť
+        if (button.read() == 0) {  // Stlačené
+            buzzer = 1;
         } else {
-            buzzer.write(0.0f);     // Off
+            buzzer = 0;
         }
 
         ThisThread::sleep_for(10ms);
