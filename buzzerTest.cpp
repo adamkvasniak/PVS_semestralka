393da0f1d048f9725f33041ee3e9c33d494d08a3
adamkvasniak <114985439+adamkvasniak@users.noreply.github.com>
2025-12-05 18:15:21 +0100

    Update buzzerTest.cpp

--- buzzerTest.cpp
+++ buzzerTest.cpp
@@ -1,10 +1,16 @@
 #include "mbed.h"
 
-DigitalOut buzzer(D3);   // Connect buzzer + wire to D3
+PwmOut buzzer(PC_8);   // PWM výstup na pin PC_8
 
 int main() {
-    buzzer = 1;          // Turn buzzer ON
+    // Nastaví frekvenciu 2 kHz (PIR buzzery najčastejšie fungujú 1–5 kHz)
+    buzzer.period(1.0f / 2000.0f);  // 2000 Hz
+
+    // 50% výkon (hlasitosť)
+    buzzer.write(0.5f);  
+
     while (1) {
+        // Buzzer stále hrá
         ThisThread::sleep_for(1s);
     }
 }
